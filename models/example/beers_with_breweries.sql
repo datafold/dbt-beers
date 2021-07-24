e33a3394e8e6afd735f56c25fa9b443c6a6892ff
Fokko Driesprong <fokko@datafold.com>
2021-07-24 20:15:07 +0200

    Changes

--- models/example/beers_with_breweries.sql
+++ models/example/beers_with_breweries.sql
@@ -2,7 +2,8 @@
     persist_docs={"relation": true, "columns": true}
 ) }}
 
-SELECT *,
-       'change' as changed
+SELECT
+    *,
+    'change' as changed
 FROM {{ ref('beers') }}
 JOIN {{ ref('breweries') }} USING (brewery_id)
