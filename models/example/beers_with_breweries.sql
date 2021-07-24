0443cc93ca9a2bb1603ab39acdfab7677ad1c9a5
Fokko Driesprong <fokko@datafold.com>
2021-07-24 19:40:31 +0200

    Trigger CI

--- models/example/beers_with_breweries.sql
+++ models/example/beers_with_breweries.sql
@@ -2,8 +2,7 @@
     persist_docs={"relation": true, "columns": true}
 ) }}
 
-SELECT
-    *,
-    'change' as changed
+SELECT *,
+       'change' as changed
 FROM {{ ref('beers') }}
 JOIN {{ ref('breweries') }} USING (brewery_id)
