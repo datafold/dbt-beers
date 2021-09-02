8ab544fefe9a37771ccb5135c5b81612f41d9cb5
Fokko Driesprong <fokko@datafold.com>
2021-09-02 18:06:09 +0200

    Add sales table (#8)
    * Add sales table
    
    * Add a nice timeseries example
    
    * Fix deps
    
    * Add pk for sales table

---
+++ models/example/orders.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized="table",
+    persist_docs={"relation": true, "columns": true}
+) }}
+
+-- We'll fake some data, since this is an example repository
+-- we want to make sure that we continue to generate data up
+-- to today
+
+{% for day_ago in range(30) %}
+    {% for order_number in range(10) %}
+      SELECT
+         TO_VARCHAR(
+            DATEADD(Day, -1 * {{ day_ago }}, current_timestamp),
+            'YYYYMMDD{{ order_number }}'
+         )::int                                                            AS order_no,
+         {% if order_number is divisibleby 13 %}
+            'PENDING'                                                      AS status,
+         {% else %}
+            'DELIVERED'                                                    AS status,
+         {% endif %}
+         DATEADD(Day, -1, current_timestamp)                               AS created_at,
+         current_timestamp                                                 AS changed_at
+
+      {% if not loop.last %}
+        UNION ALL
+      {% endif %}
+    {% endfor %}
+
+    {% if not loop.last %}
+        UNION ALL
+    {% endif %}
+{% endfor %}
