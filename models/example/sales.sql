e7250aee1dd059cbd71a4c2f39d06b34edd80777
Fokko Driesprong <fokko@datafold.com>
2021-09-03 17:51:16 +0200

    Only take the delivered orders into account (#9)

--- models/example/sales.sql
+++ models/example/sales.sql
@@ -27,3 +27,5 @@ SELECT
 FROM {{ ref('orders') }} orders
 JOIN {{ ref('order_lines') }} order_lines USING (order_no)
 JOIN {{ ref('beers_with_breweries') }} beers_with_breweries USING (beer_id)
+
+WHERE orders.status = 'DELIVERED'
