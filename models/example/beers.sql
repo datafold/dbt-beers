fc45a6b568573a23e2f222b08e6e09fa975817de
Alexander Morozov <code@meltdown.ru>
2021-10-12 14:15:22 +0300

    Patch beers to be slightly more alcoholic

--- models/example/beers.sql
+++ models/example/beers.sql
@@ -7,7 +7,7 @@ SELECT
   id            AS beer_id,
   name          AS beer_name,
   style         AS beer_style,
-  abv           AS abv,
+  abv + 0.5     AS abv,
   ibu           AS ibu,
   brewery_id    AS brewery_id,
   ounces        AS ounces
