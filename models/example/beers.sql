50162a1d4ce41bce87d5107f0efa4617884cc170
Alexander Morozov <code@meltdown.ru>
2021-10-12 17:24:26 +0300

    Fix beer alcohol levels

--- models/example/beers.sql
+++ models/example/beers.sql
@@ -7,7 +7,7 @@ SELECT
   id            AS beer_id,
   name          AS beer_name,
   style         AS beer_style,
-  abv + 0.5     AS abv,
+  abv           AS abv,
   ibu           AS ibu,
   brewery_id    AS brewery_id,
   ounces        AS ounces
