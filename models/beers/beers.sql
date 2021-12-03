e25404172c0584d8882ad4999beb8afe01fd5e4b
Fokko Driesprong <fokko@datafold.com>
2021-12-03 13:16:10 +0100

    Add missing order and normalize beer names

--- models/beers/beers.sql
+++ models/beers/beers.sql
@@ -4,12 +4,12 @@
 ) }}
 
 SELECT
-  id            AS beer_id,
-  TRIM(name)    AS beer_name,
-  style         AS beer_style,
-  abv           AS abv,
-  ibu           AS ibu,
-  brewery_id    AS brewery_id,
-  ounces        AS ounces
+  id                                             AS beer_id,
+  TRIM(REGEXP_REPLACE(name, '\\([0-9]+\\)', '')) AS beer_name,
+  style                                          AS beer_style,
+  abv                                            AS abv,
+  ibu                                            AS ibu,
+  brewery_id                                     AS brewery_id,
+  ounces                                         AS ounces
 FROM
   {{ ref('seed_beers') }}
