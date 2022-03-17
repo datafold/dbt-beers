b17baeb56ab85b5048a8529cefcb0bd25ea39767
Gleb Mezhanskiy <gleb@Glebs-MacBook-Pro.local>
2022-03-17 15:13:21 -0700

    refactor promos to simplify

--- models/beers/promo_deliveries.sql
+++ models/beers/promo_deliveries.sql
@@ -4,36 +4,28 @@
     transient=false
 ) }}
 
-SELECT
- customer_id,
- 'Hoppy' AS promo
-FROM {{ ref('orders') }}
-WHERE customer_id IN(
-    SELECT customer_id 
-      FROM {{ ref('orders') }} o
-      JOIN {{ ref('order_lines') }} ol
-      USING (order_no)
-      JOIN {{ ref('beers') }} b
-      USING(beer_id)
-      GROUP BY 1
-      HAVING COUNT(CASE WHEN b.bitterness = 'Hoppy' THEN 1 END)
-             >
-             COUNT(CASE WHEN b.bitterness = 'Malty' THEN 1 END))
+SELECT customer_id,
+       'Hoppy' AS promo    
+  FROM {{ ref('orders') }} o
+  JOIN {{ ref('order_lines') }} ol
+  USING (order_no)
+  JOIN {{ ref('beers') }} b
+  USING(beer_id)
+  GROUP BY 1
+  HAVING COUNT(CASE WHEN b.bitterness = 'Hoppy' THEN 1 END)
+         >
+         COUNT(CASE WHEN b.bitterness = 'Malty' THEN 1 END)
 
 UNION
 
-SELECT
- customer_id,
- 'Malty' AS promo
-FROM {{ ref('orders') }}
-WHERE customer_id IN(
-    SELECT customer_id
-      FROM {{ ref('orders') }} o
-      JOIN {{ ref('order_lines') }} ol
-      USING (order_no)
-      JOIN {{ ref('beers') }} b
-      USING(beer_id)
-      GROUP BY 1
-      HAVING COUNT(CASE WHEN b.bitterness = 'Malty' THEN 1 END)
-             >
-             COUNT(CASE WHEN b.bitterness = 'Hoppy' THEN 1 END))
+SELECT customer_id,
+       'Malty' AS promo
+  FROM {{ ref('orders') }} o
+  JOIN {{ ref('order_lines') }} ol
+  USING (order_no)
+  JOIN {{ ref('beers') }} b
+  USING(beer_id)
+  GROUP BY 1
+  HAVING COUNT(CASE WHEN b.bitterness = 'Malty' THEN 1 END)
+         >
+         COUNT(CASE WHEN b.bitterness = 'Hoppy' THEN 1 END)
