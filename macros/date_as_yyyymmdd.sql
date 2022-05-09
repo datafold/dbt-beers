e4e59089d6334ae038e4256de0389d4650adb476
Anatoly Scherbakov <altaisoft@gmail.com>
2022-05-09 12:49:04 +0400

    [DAT-3225]: Refactor macros to use `adapter.dispatch()`

--- macros/date_as_yyyymmdd.sql
+++ macros/date_as_yyyymmdd.sql
@@ -1,10 +1,25 @@
+{#
+Convert a date to a YYYYMMDD string.
+
+This macro is needed because the date format string differs from one database backend to another.
+#}
 {%- macro date_as_yyyymmdd() -%}
+    {{ return(adapter.dispatch('date_as_yyyymmdd')()) }}
+{%- endmacro -%}
+
+
+{%- macro default__date_as_yyyymmdd() -%}
     {{ date_format() }}(
         {{ caller() }},
-        {% if target.type == 'databricks' %}
-            'yMMdd'
-        {% else %}
-            'YYYYMMDD'
-        {% endif %}
+        'YYYYMMDD'
+    )
+{%- endmacro -%}
+
+
+{%- macro databricks__date_as_yyyymmdd() -%}
+    {{ date_format() }}(
+        {{ caller() }},
+        {# Reference: https://docs.databricks.com/sql/language-manual/sql-ref-datetime-pattern.html #}
+        'yMMdd'
     )
 {%- endmacro -%}
--- macros/date_format.sql
+++ macros/date_format.sql
@@ -9,11 +9,17 @@ There are certain differences in the format string treatment:
 It is thus recommended to only use the recognized formatting symbols in format expressions.
 #}
 {%- macro date_format() -%}
-    {%- if target.type == 'databricks' -%}
-        {# https://docs.databricks.com/sql/language-manual/functions/date_format.html #}
-        DATE_FORMAT
-    {%- else -%}
-        {# https://docs.snowflake.com/en/sql-reference/functions/to_char.html #}
-        TO_VARCHAR
-    {%- endif -%}
+    {{ return(adapter.dispatch('date_format')()) }}
+{%- endmacro -%}
+
+
+{%- macro databricks__date_format() -%}
+    {# https://docs.databricks.com/sql/language-manual/functions/date_format.html #}
+    DATE_FORMAT
+{%- endmacro -%}
+
+
+{%- macro default__date_format() -%}
+    {# https://docs.snowflake.com/en/sql-reference/functions/to_char.html #}
+    TO_VARCHAR
 {%- endmacro -%}
