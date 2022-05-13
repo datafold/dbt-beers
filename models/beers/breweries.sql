1c24b5572b31c4ccf2c747861bad9b225d9ed814
Gerard Toonstra <gerard@datafold.com>
2022-05-13 17:34:53 +0200

    [test-gerard]

--- models/beers/breweries.sql
+++ models/beers/breweries.sql
@@ -8,6 +8,6 @@ SELECT
   trim(name)            AS brewery_name,
   trim(city)            AS brewery_city,
   trim(state)           AS brewery_state,
-  'USA'                 AS brewery_country
+  'AA'                 AS brewery_country
 FROM
   {{ ref('seed_breweries') }}
