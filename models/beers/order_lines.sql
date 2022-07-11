f685cabd6924abbce4c358cd5e337e64d3c2ac55
Alexey M <95286019+teraamp@users.noreply.github.com>
2022-07-11 16:07:38 +1000

    Update order_lines.sql
    update type conversion

--- models/beers/order_lines.sql
+++ models/beers/order_lines.sql
@@ -12,13 +12,13 @@ WITH day_table AS (SELECT seq4() day_ago from table(generator(rowcount=>30))),
 order_table  AS (SELECT seq4() order_number from table(generator(rowcount=>10))),
 line_table  AS (SELECT seq4() line_no from table(generator(rowcount=>3))) 
 
-SELECT TO_VARCHAR(
+SELECT {{ date_format() }}(
            DATEADD(Day, -1 * day_ago , current_timestamp),
-           concat('YYYYMMDD',to_varchar(order_number))
+           concat('YYYYMMDD',cast(order_number as varchar))
        )                                                   AS order_no,
-       TO_VARCHAR(
+       {{ date_format() }}(
            DATEADD(Day, -1 *  day_ago, current_timestamp),
-           CONCAT('YYYYMMDD', TO_VARCHAR(order_number),to_varchar(line_no))
+           CONCAT('YYYYMMDD', cast(order_number as varchar),cast(line_no))
        )                                                   AS order_line,
        (
        -- Deterministically select a random beer
