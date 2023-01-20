d759b2c3f328de8c00770aceefcae30f6a791014
Leo Folsom <leoebfolsom@gmail.com>
2023-01-20 11:04:18 -0800

    remove limit from sales

--- tests/diff_orders.sql
+++ tests/diff_orders.sql
@@ -3,7 +3,8 @@
         a_relation=ref('orders'),
         b_relation=api.Relation.create(database='INTEGRATION', schema='BEERS', identifier='orders'), 
         exclude_columns=['customer_id', 'changed_at'], 
-        primary_key='order_no'
+        primary_key='order_no',
+        summarize=false
     ) 
 }}
 where not perfect_match
