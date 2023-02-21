8b67f17a992d0262e8d6fc5dc2e58361203495d2
Leo Folsom <leoebfolsom@gmail.com>
2023-02-21 14:25:42 -0800

    fix audit_helper test

--- tests/diff_beers.sql
+++ tests/diff_beers.sql
@@ -1,7 +1,7 @@
 {{ 
     audit_helper.compare_all_columns(
         a_relation=ref('beers'),
-        b_relation=api.Relation.create(database='INTEGRATION', schema='BEERS', identifier='beers'), 
+        b_relation=api.Relation.create(database='INTEGRATION', schema='INTEGRATION', identifier='BEERS'), 
         exclude_columns=[], 
         primary_key='beer_id',
         summarize=false
